0fe78a6b76fca93461ae4fbd7e0c1351390a9de5
Elliot Metsger <emetsger@jhu.edu>
2018-10-29 08:35:16 -0400

    Log git properties on startup.

--- notification-boot/src/main/java/org/dataconservancy/pass/notification/app/NotificationApp.java
+++ notification-boot/src/main/java/org/dataconservancy/pass/notification/app/NotificationApp.java
@@ -16,10 +16,17 @@
 package org.dataconservancy.pass.notification.app;
 
 import org.dataconservancy.pass.notification.app.config.SpringBootNotificationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 
 /**
  * Spring Boot entry point for launching Notification Services.
@@ -32,13 +39,47 @@ import org.springframework.context.annotation.Import;
 @EnableAspectJAutoProxy
 public class NotificationApp {
 
+    private static final Logger LOG = LoggerFactory.getLogger(NotificationApp.class);
+
+    private static final String GIT_BUILD_VERSION_KEY = "git.build.version";
+
+    private static final String GIT_COMMIT_HASH_KEY = "git.commit.id.abbrev";
+
+    private static final String GIT_COMMIT_TIME_KEY = "git.commit.time";
+
+    private static final String GIT_DIRTY_FLAG = "git.dirty";
+
+    private static final String GIT_BRANCH = "git.branch";
+
+    private static final String GIT_PROPERTIES_RESOURCE_PATH = "/git.properties";
+
     /**
      * Spring Boot entry point.
      *
      * @param args command line args to be processed
      */
     public static void main(String[] args) {
-        System.exit(1);
+        URL gitPropertiesResource = NotificationApp.class.getResource(GIT_PROPERTIES_RESOURCE_PATH);
+        if (gitPropertiesResource == null) {
+            LOG.info(">>>> Starting DepositServices (no Git commit information available)");
+        } else {
+            Properties gitProperties = new Properties();
+            try {
+                gitProperties.load(gitPropertiesResource.openStream());
+                boolean isDirty = Boolean.valueOf(gitProperties.getProperty(GIT_DIRTY_FLAG));
+
+                LOG.info(">>>> Starting Notification Services (version: {} branch: {} commit: {} commit date: {})",
+                        gitProperties.get(GIT_BUILD_VERSION_KEY), gitProperties.get(GIT_BRANCH), gitProperties.get(GIT_COMMIT_HASH_KEY), gitProperties.getProperty(GIT_COMMIT_TIME_KEY));
+
+                if (isDirty) {
+                    LOG.warn(">>>> ** Notification Services was compiled from a Git repository with uncommitted changes! **");
+                }
+            } catch (IOException e) {
+                LOG.warn(">>>> Error parsing Notification Services git information (" + GIT_PROPERTIES_RESOURCE_PATH + " could not be parsed: " + e.getMessage() + ")");
+            }
+        }
+
+        SpringApplication.run(NotificationApp.class, args);
     }
 
 }
